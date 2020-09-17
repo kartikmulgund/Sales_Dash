79bb805572b7b53a5e9e5b31f1effcc9935b96c3
Kartik Mulgund <kartikmulgund@gmail.com>
2020-09-17 14:25:34 +0530

    redux fix

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,14 +2,16 @@ import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers/rootReducer'
 
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+  devTools = (a) => a
+}
+
 export default function configureStore() {
   const store = createStore(
     rootReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    compose(applyMiddleware(thunk), devTools)
   )
   return store
 }
